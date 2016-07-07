0d5d28ffdbab72418e0c272b19edb84009868111
tuanttps03471 <tuanttps03741@fpt.edu.vn>
2016-07-07 08:34:04 +0700

    Inf205
    asd

---
+++ frmXemsanpham.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data.DataSet
+Public Class frmXemsanpham
+    Dim db As New DataTable
+    Dim chuoiketnoi As String = "workstation id=tuantpps03715.mssql.somee.com;packet size=4096;user id=tranphantuan0502_SQLLogin_1;pwd=v9r2mtvsdq;data source=tuantpps03715.mssql.somee.com;persist security info=False;initial catalog=tuantpps03715"
+    Dim connect As SqlConnection = New SqlConnection(chuoiketnoi)
+    Private Sub btnXemall_Click(sender As Object, e As EventArgs) Handles btnXemall.Click
+        Dim hienthi As New Class1
+        dgvXemsp.DataSource = hienthi.Loadsanpham.Tables(0)
+    End Sub
+
+    Private Sub btnXem_Click(sender As Object, e As EventArgs) Handles btnXem.Click
+        Dim connect As SqlConnection = New SqlConnection(chuoiketnoi)
+        connect.Open()
+        Dim timkiem As SqlDataAdapter = New SqlDataAdapter("select Sanpan.MASP as 'Mã sản phẩm',Sanpan.TENSP as 'Tên sản phẩm', LOAISANPHAM.MALOAI as 'Mã Loại', LOAISANPHAM.TENLOAI as 'Tên Loại',SANPHAM.SOLUONG as 'Số lượng' from SANPHAM inner join LOAISANPHAM on SANPHAM.MASP = LOAISANPHAM.MASP where SANPHAM.MASP ='" & txtMASP.Text & "' ", connect)
+        Try
+            If txtMASP.Text = "" Then
+                MessageBox.Show("Bạn cần nhập mã sản phẩm", "Nhập thiếu", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Else
+                db.Clear()
+                dgvXemsp.DataSource = Nothing
+                timkiem.Fill(db)
+                If db.Rows.Count > 0 Then
+                    dgvXemsp.DataSource = db.DefaultView
+                    txtMASP.Text = Nothing
+                Else
+                    MessageBox.Show("Không tìm được")
+                    txtMASP.Text = Nothing
+                End If
+            End If
+            connect.Close()
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub btnDong_Click(sender As Object, e As EventArgs) Handles btnDong.Click
+        Me.Close()
+    End Sub
+
+    Private Sub frmXemsanpham_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
